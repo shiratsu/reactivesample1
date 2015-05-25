50d0067de41abbf870844eb23dfa37882515f22a
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-05-26 07:24:23 +0900

    一旦これで

---
+++ ReactiveSample1/ReactiveSample1-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ReactiveSample1-bridging-Header.h
+//  ReactiveSample1
+//
+//  Created by HIRATSUKA SHUNSUKE on 2015/05/26.
+//  Copyright (c) 2015年 平塚 俊輔. All rights reserved.
+//
+
+#ifndef ReactiveSample1_ReactiveSample1_bridging_Header_h
+#define ReactiveSample1_ReactiveSample1_bridging_Header_h
+
+
+#endif
+#import <ReactiveCocoa/ReactiveCocoa.h>
--- ReactiveSample1/ViewController.swift
+++ ReactiveSample1/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var s1: UISegmentedControl!
+    @IBOutlet weak var s2: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
+struct RAC  {
+    var target : NSObject!
+    var keyPath : String!
+    var nilValue : AnyObject!
+    
+    init(_ target: NSObject!, _ keyPath: String, nilValue: AnyObject? = nil) {
+        self.target = target
+        self.keyPath = keyPath
+        self.nilValue = nilValue
+    }
+    
+    func assignSignal(signal : RACSignal) {
+        signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
+    }
+}
+
+operator infix ~> {}
+@infix func ~> (signal: RACSignal, rac: RAC) {
+    rac.assignSignal(signal)
+}
